8ff6261727704c2822626b105fff713185d4bde7
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-25 16:44:06 +0900

    jQueryのスムーススクロールの編集

--- app/javascript/anime.js
+++ app/javascript/anime.js
@@ -1,15 +1,26 @@
 window.onload=function(){
   
-    var scroll = document.querySelectorAll('.anime_up');
+  var scroll = document.querySelectorAll('.anime_up');
        
-    var Animation = function() {
-      for(var i = 0; i < scroll.length; i++) {
-      var triggerMargin = 50;
-      if (window.innerHeight > scroll[i].getBoundingClientRect().top + triggerMargin) {
-      scroll[i].classList.add('show');
-      }
-      }
-      }
-      window.addEventListener('scroll', Animation);
+  var Animation = function() {
+    for(var i = 0; i < scroll.length; i++) {
+    var triggerMargin = 50;
+    if (window.innerHeight > scroll[i].getBoundingClientRect().top + triggerMargin) {
+    scroll[i].classList.add('show');
+    }
+    }
+    }
+    window.addEventListener('scroll', Animation);
+
+    window.addEventListener('DOMContentLoaded', function(){
+      $('a[href^=#]').click(function() {
+        var speed = 100;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
+        return false;
+      });
+    });
 
 }
